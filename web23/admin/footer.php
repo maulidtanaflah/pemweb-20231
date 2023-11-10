735c1797b232e2468e21b2bd2e45cbcb0712cf95
Maulidta <99494424+maulidtanaflah@users.noreply.github.com>
2023-11-10 14:06:42 +0700

    Delete web23 directory

--- web23/admin/footer.php
+++
@@ -1,6 +0,0 @@
-<footer>
-    <div class="text-center text-white p3" style="background-color:black">
-     &copy; 2023
-     <a class="text-center" href="https://namaanda.com"> NamaAnda.com</a>
-    </div>
-</footer>
